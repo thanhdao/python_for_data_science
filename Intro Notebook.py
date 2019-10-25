b3ee4a20058f0714cd66f9ec6959d23efce85629
Thanh Dao <thanhdd1@gmail.com>
2019-10-25 20:18:23 +0900

    notebook

---
+++ Intro Notebook.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# Below is how we calculate the number of second in a leap year
+
+# In[11]:
+
+
+365 * 24 * 60 * 60
+
+
+# In[12]:
+
+
+_ / 1e6
+
+
+# In[13]:
+
+
+366 * 24 * 60 * 60
+
+
+# In[14]:
+
+
+_ / 1e6
+
+
+# In[16]:
+
+
+x = 4 + 3
+print(x )
+
+
+# # This is a markdown cell.
+# 
+# ## This is heading 2.
+# ### This is heading 3.
+# 
+# Hi!
+# * ONe fish
+# * Two fish
+# 
+# **example text**
+# 
+# *example text*
+# 
+# 
+# http://google.com
+# 
+# 
+
+# This is a **Latex** equation:
+# 
+# $\int_0^\infty x^{-\alpha}$
+
+# In[24]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from matplotlib.pyplot import plot
+
+plot([0,1,0,1])
+
+
+# This is a plot using matplotlib of a vector.
+
+# In[ ]:
+
+
+
+
